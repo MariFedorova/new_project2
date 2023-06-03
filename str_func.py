89ba8633e6c20e17d65008ad4f817a99c1ea14d4
Mariia Fedorova <000maria.fed@gmail.com>
2023-06-03 11:20:59 +0300

    докстринги и вторая функция

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def func(text):
+    """функция перевода текста в верхний регистр"""
     return text.upper()
+
+def funk_text(text):
+    """функция перевода первых букв в верхний регистр"""
+    return text.title()
